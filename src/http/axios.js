cd7bfefebe89090380ccbbe1233e86bdee7a0ebb
KhanhHH <khanhhh@mirabo-global.com>
2023-11-03 12:00:17 +0700

    feat: add cookie to request

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 // set new axios instance
 export default axios.create({
-    withCredentials: true
+  withCredentials: true,
+  headers: {
+    session: Cookies.get('sid'),
+  },
 });

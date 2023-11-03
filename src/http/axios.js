4aae314cf9e0354ebd56a6850f3bc4011a69fb8d
KhanhHH <khanhhh@mirabo-global.com>
2023-11-03 12:56:29 +0700

    feat: revert

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 // set new axios instance
 export default axios.create({
   withCredentials: true,
-  headers: {
-    session: Cookies.get('sid'),
-  },
 });
